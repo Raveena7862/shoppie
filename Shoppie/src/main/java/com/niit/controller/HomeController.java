d857b5b1076344a0c760c9fa7026cea705ee8ed4
Raveena7862 <raveenayadav94@gmail.com>
2017-02-24 14:44:22 +0530

    first commit
    login and registration page

---
+++ Shoppie/src/main/java/com/niit/controller/HomeController.java
@@ -0,0 +1,112 @@
+package com.niit.controller;
+
+import java.util.List;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.niit.model.Customer;
+import com.niit.service.CustomerService;
+
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/")
+    public String home() {
+        return "home";
+    }
+	
+	@RequestMapping("/admin")
+    public String adminpage() {
+        return "admin";
+    }
+	
+	@RequestMapping("/home")
+    public String homepage() {
+        return "home";
+    }
+    
+   /* @RequestMapping("/login")
+    public String loginpage() {
+        return "login";
+    }
+    */
+   /* @RequestMapping("/register")
+    public String registerpage() {
+        return "register";
+    }*/
+    
+    @RequestMapping("/registersuccess")
+    public String registersuccesspage() {
+        return "registersuccess";
+    }
+    
+    @RequestMapping("/login")
+    public String login(@RequestParam(value="error", required = false) String error, @RequestParam(value="logout",
+            required = false) String logout, Model model) {
+        if (error!=null) {
+            model.addAttribute("error", "Invalid username and password");
+        }
+
+        if(logout!=null) {
+            model.addAttribute("msg", "You have been logged out successfully.");
+        }
+
+        return "login";
+    }
+    
+    @Autowired
+    private CustomerService customerService;
+
+    @RequestMapping("/register")
+    public String registerCustomer(Model model) {
+
+        Customer customer = new Customer();
+       
+        model.addAttribute("customer", customer);
+
+        return "register";
+
+    }
+
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public String registerCustomerPost(@Valid @ModelAttribute("customer") Customer customer, BindingResult result,
+                                       Model model) {
+
+        if (result.hasErrors()) {
+            return "register";
+        }
+
+        List<Customer> customerList=customerService.getAllCustomers();
+
+        for (int i=0; i< customerList.size(); i++) {
+            if(customer.getCustomerEmail().equals(customerList.get(i).getCustomerEmail())) {
+                model.addAttribute("emailMsg", "Email already exists");
+
+                return "register";
+            }
+
+            if(customer.getUsername().equals(customerList.get(i).getUsername())) {
+                model.addAttribute("usernameMsg", "Username already exists");
+
+                return "register";
+            }
+        }
+
+        customer.setEnabled(true);
+        customerService.addCustomer(customer);
+
+        return "registersuccess";
+
+    }
+
+
+}
---
+++ Shoppie/src/main/java/com/niit/service/CustomerService.java
@@ -0,0 +1,16 @@
+package com.niit.service;
+
+import com.niit.model.Customer;
+
+import java.util.List;
+
+public interface CustomerService {
+
+    void addCustomer (Customer customer);
+
+    Customer getCustomerById (int customerId);
+
+    List<Customer> getAllCustomers();
+
+    Customer getCustomerByUsername (String username);
+}
